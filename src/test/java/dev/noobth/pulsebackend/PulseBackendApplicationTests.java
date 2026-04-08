726945fea32729d1a63f175cef6b5530c276224c
SeungYeop Ryu <n006h3417h@gmail.com>
2026-04-08 20:24:00 +0900

    init

---
+++ src/test/java/dev/noobth/pulsebackend/PulseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.noobth.pulsebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
